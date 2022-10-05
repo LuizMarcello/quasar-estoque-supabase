a82c2da86fb5be7cef9aff98014e8f0fd38b038a
luizmarcello <luizmarcello.vmo@hotmail.com>
2022-10-05 08:31:32 -0300

    Londrina:05.10.22_08:31hs

---
+++ src/pages/Login.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page padding>
+    <p class="text-h6">Login</p>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "PageLogin",
+});
+</script>
--- src/router/routes.js
+++ src/router/routes.js
@@ -2,7 +2,7 @@
 const routes = [
   {
     path: '/',
-    component: () => import('layouts/MainLayout.vue'),
+    component: () => import('layouts/MainLayoutLogado.vue'),
     children: [
       { path: '', component: () => import('pages/IndexPage.vue') }
     ]
