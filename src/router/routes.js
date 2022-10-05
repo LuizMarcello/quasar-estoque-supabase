386d552891d529220b810da221adf05465aa261f
luizmarcello <luizmarcello.vmo@hotmail.com>
2022-10-05 17:36:48 -0300

    Rolândia:05.10.22_17:36hs

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,19 +1,33 @@
-
 const routes = [
   {
-    path: '/',
-    component: () => import('layouts/MainLayout.vue'),
+    path: "/",
+    component: () => import("layouts/LoginLayout.vue"),
     children: [
-      { path: '', component: () => import('pages/IndexPage.vue') }
-    ]
+      {
+        path: "",
+        name: "loginDefault",
+        component: () => import("pages/Login.vue"),
+      },
+      {
+        path: "login",
+        name: "login",
+        component: () => import("pages/Login.vue"),
+      },
+    ],
+  },
+
+  {
+    path: "/",
+    component: () => import("layouts/MainLayout.vue"),
+    children: [{ path: "", component: () => import("pages/IndexPage.vue") }],
   },
 
   // Always leave this as last one,
   // but you can also remove it
   {
-    path: '/:catchAll(.*)*',
-    component: () => import('pages/ErrorNotFound.vue')
-  }
-]
+    path: "/:catchAll(.*)*",
+    component: () => import("pages/ErrorNotFound.vue"),
+  },
+];
 
-export default routes
+export default routes;
