db0ab0df6b65ef68d0a01b5e3b46724723881c50
luizmarcello <luizmarcello.vmo@hotmail.com>
2022-10-06 09:23:45 -0300

    Londrina:06.09.22_09:23hs

--- src/layouts/LoginLayout.vue
+++ src/layouts/LoginLayout.vue
@@ -16,15 +16,10 @@
 
 <script>
 import { defineComponent } from "vue";
-<<<<<<< HEAD
-export default defineComponent({
-  name: "LoginLayout",
-=======
 
 export default defineComponent({
   name: "LoginLayout",
 
->>>>>>> a82c2da86fb5be7cef9aff98014e8f0fd38b038a
   setup() {
     return {};
   },
