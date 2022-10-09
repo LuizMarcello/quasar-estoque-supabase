67a7c9049faec1117d1e848cdd5516c1a9ca09a4
luizmarcello <luizmarcello.vmo@hotmail.com>
2022-10-09 13:08:05 -0300

    Londrina:09.10.22_13:07hs

--- src/pages/ResetPassword.vue
+++ src/pages/ResetPassword.vue
@@ -4,7 +4,8 @@
       <p class="col-12 text-h5 text-center">Resetar a senha</p>
       <div class="col-md-4 col-sm-6 col-xs-10 q-gutter-y-md">
 
-        <q-input label="Nova senha" v-model="password" />
+        <q-input label="Nova senha" v-model="password" lazy-rules :rules="[
+        val => (val && val.length >= 6) || 'Informe a nova senha (mínimo 6 digitos)']" />
 
         <div class="full-width q-pt-md q-gutter-y-sm">
           <q-btn label="Confirmar nova senha" color="primary" class="full-width" outline rounded size="md"
@@ -18,6 +19,7 @@
 
 <script>
 import { defineComponent, ref } from 'vue';
+import useNotify from "src/composables/UseNotify";
 import useAuthUser from 'src/composables/UseAuthUser';
 import { useRouter, useRoute } from 'vue-router';
 /* Diferença: */
@@ -29,19 +31,24 @@ export default defineComponent({
 
   setup() {
     const { resetPassword } = useAuthUser();
+    const { notifyError, notifySuccess } = useNotify()
     /* Para fazer o redirecionamento */
     const router = useRouter();
     /* Pegando a rota atual para extrair o token dela */
     const route = useRoute();
+
     const token = route.query.token;
 
     const password = ref('');
 
     const handlePasswordReset = async () => {
-      await resetPassword(token, password.value)
-      router.push({
-        name: 'login'
-      });
+      try {
+        await resetPassword(token, password.value)
+        notifySuccess('Nova senha enviada com sucesso')
+        router.push({ name: 'login' })
+      } catch (error) {
+        notifyError(error.message)
+      }
     }
 
     return {
