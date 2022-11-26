4c1032733505ac1b2985549faf59352194ea6bb2
luizmarcello <luizmarcello.vmo@hotmail.com>
2022-11-26 14:26:02 -0300

    Londrina:26.11.22_14:25hs

--- quasar.config.js
+++ quasar.config.js
@@ -161,7 +161,7 @@ module.exports = configure(function (ctx) {
       workboxPluginMode: "GenerateSW", // 'GenerateSW' or 'InjectManifest'
       workboxOptions: {
         skipWaiting: true,
-        clientClaim: true
+        clientsClaim: true
       }, // only for GenerateSW
 
       // for the custom service worker ONLY (/src-pwa/custom-service-worker.[js|ts])
